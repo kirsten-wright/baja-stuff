8ce5b131b890be61c018c60ba221bc4cba6a8f74
Everett Periman <ejpman@gmail.com>
2020-01-11 15:21:28 -0500

    All done(kind of)

--- 1_7_engine_read.ino/1_7_engine_read.ino.ino
+++ 1_7_engine_read.ino/1_7_engine_read.ino.ino
@@ -7,23 +7,35 @@ volatile unsigned long time2b;
 volatile unsigned long deltaL;
 volatile double placehold;
 volatile int gateL = 0;
+volatile double rpm;
+volatile double last_rpm;
 
 void setup(){
   Serial.begin(1000000);
   attachInterrupt(digitalPinToInterrupt(3), ignition, FALLING);
   time1b = micros();
   }
-void loop(){}
+void loop(){
+  if(millis()%10 == 0){
+  if((rpm >5000.0){rpm = last_rpm;}
+  Serial.println(rpm);
+  }
+  }
+
 int ignition(){
-  
 if(gateL==0){gateL=1;}
 else if(gateL==1){
   time2b = micros();
-  deltaL = time2b-time1b;
-  placehold = deltaL/1000000.0;
-  placehold = 60.0/(placehold);
+  last_rpm = rpm;
+  rpm = rpm_calc(time2b,time1b);
   time1b = time2b;
   gateL=0; 
-  Serial.println(placehold);
 }
+}
+
+double rpm_calc(unsigned long timeA, unsigned long timeB){
+  deltaL = timeA-timeB;
+  placehold = deltaL/1000000.0;
+  placehold = 60.0/(placehold);
+  return placehold;
 }
