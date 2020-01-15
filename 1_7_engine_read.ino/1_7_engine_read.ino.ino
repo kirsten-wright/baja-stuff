6a4493eb47a6c046fac395cf9fcdd89e072ab1ab
Everett Periman <ejpman@gmail.com>
2020-01-14 19:37:21 -0500

    more updates

--- 1_7_engine_read.ino/1_7_engine_read.ino.ino
+++ 1_7_engine_read.ino/1_7_engine_read.ino.ino
@@ -2,25 +2,31 @@
 #include <SoftwareSerial.h> 
 //Pin 2 is attached to gear tooth sensor
 //Pin 3 is attached to engine rpm
+#define drivegear 61;
+#define cvtshaft 18;
 volatile unsigned long time1b;
 volatile unsigned long time2b;
 volatile unsigned long deltaL;
 volatile double placehold;
 volatile int gateL = 0;
+volatile double gear_vel;
 volatile double rpm;
 volatile double last_rpm;
+unsigned long timestamp;
 
 void setup(){
   Serial.begin(1000000);
   attachInterrupt(digitalPinToInterrupt(3), ignition, FALLING);
   time1b = micros();
-  }
+}
+
 void loop(){
   if(millis()%10 == 0){
-  if((rpm >5000.0){rpm = last_rpm;}
-  Serial.println(rpm);
+  if((rpm > 1.5*(last_rpm + 1.0)){rpm = last_rpm;}
+  timestamp = micros();
+  Serial.println("%lu,%lf,%lf",timestamp,rpm,gear_vel);
   }
-  }
+}
 
 int ignition(){
 if(gateL==0){gateL=1;}
@@ -30,7 +36,7 @@ else if(gateL==1){
   rpm = rpm_calc(time2b,time1b);
   time1b = time2b;
   gateL=0; 
-}
+  }
 }
 
 double rpm_calc(unsigned long timeA, unsigned long timeB){
@@ -39,3 +45,16 @@ double rpm_calc(unsigned long timeA, unsigned long timeB){
   placehold = 60.0/(placehold);
   return placehold;
 }
+
+/*
+int gear(){
+if(gateL==0){gateL=1;}
+else if(gateL==1){
+  time2b = micros();
+  last_rpm = rpm;
+  rpm = rpm_calc(time2b,time1b);
+  time1b = time2b;
+  gateL=0; 
+  }
+}
+*/
