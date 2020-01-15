9859ba5722f1f6cf0dd42e2df5b00535a19c1b6f
Everett Periman <ejpman@gmail.com>
2020-01-14 20:14:39 -0500

    more stuff

--- 1_7_engine_read.ino/1_7_engine_read.ino.ino
+++ 1_7_engine_read.ino/1_7_engine_read.ino.ino
@@ -4,28 +4,42 @@
 //Pin 3 is attached to engine rpm
 #define drivegear 61;
 #define cvtshaft 18;
-volatile unsigned long time1b, time2b, deltaL, deltaG;
-volatile double placehold, gear_vel, rpm, last_rpm;
+volatile unsigned long time1b, time2b, time1a, time2a, deltaL, deltaG;
+volatile double placehold, vel, last_vel, rpm, last_rpm;
 volatile int gateL = 0;
+volatile int gateG = 0;
+volatile double ;
+volatile double ;
+volatile double ;
 unsigned long timestamp;
 
 void setup(){
   Serial.begin(1000000);
   attachInterrupt(digitalPinToInterrupt(3), ignition, FALLING);
+  attachInterrupt(digitalPinToInterrupt(2), rotation, FALLING);
   time1b = micros();
+  time1a = micros();
 }
 
 void loop(){
   if(millis()%10 == 0){
   if(rpm > (1.5*(last_rpm + 1.0))){rpm = last_rpm;}
   timestamp = micros();
-  
   Serial.print(timestamp);
   Serial.print(",");
   Serial.print(rpm);
   Serial.print(",");
-  Serial.println(gear_vel);
+  Serial.println(vel);
+  }
+}
 
+int rotation(){
+if(gateG==0){gateG=1;}
+else if(gateG==1){
+  time2a = micros();
+  last_vel = vel;
+  time1a = time2a;
+  gateG=0; 
   }
 }
 
@@ -47,13 +61,8 @@ double rpm_calc(unsigned long timeA, unsigned long timeB){
   return placehold;
 }
 
-int gear(){
-if(gateG==0){gateG=1;}
-else if(gateG==1){
-  time2b = micros();
-  last_rpm = rpm;
-  rpm = rpm_calc(time2b,time1b);
-  time1b = time2b;
-  gateL=0; 
-  }
+double vel_calc(unsigned long timeA, unsigned long timeB){
+  deltaG = timeA-timeB;
+
+  return placehold;
 }
